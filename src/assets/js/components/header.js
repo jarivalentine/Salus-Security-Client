c670d63590345e62dc389560e24f9afe1b842c74
jari.valentine <jari.valentine@student.howest.be>
2022-11-10 12:02:29 +0100

    index page setup

---
+++ src/assets/js/components/header.js
@@ -0,0 +1,8 @@
+export default {
+    template: `
+        <header>
+            <span id="profile" class="fa-solid fa-circle-user fa-2x"></span>
+            <span id="setting" class="fa-solid fa-wrench fa-2x"></span>
+        </header>
+    `
+}
