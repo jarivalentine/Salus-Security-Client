eb087829bf8149fdb5737850450f74dcf8df6b5a
jari.valentine <jari.valentine@student.howest.be>
2022-11-11 14:17:21 +0100

    header css

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,7 +1,17 @@
 export default {
+    data() {
+        return {
+            firstname: 'Liam',
+            tag: 'Gardian Angel'
+        }
+    },
     template: `
         <header>
-            
+            <div>
+                <p>{{ firstname }}</p>
+                <p class="tag">{{ tag }}</p>
+            </div>
+            <a href="./settings.html"></a>
         </header>
     `
 }
