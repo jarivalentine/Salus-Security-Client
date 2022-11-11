25993fe4ba518ec78dad07afdeef58ecf6fb011c
jari.valentine <jari.valentine@student.howest.be>
2022-11-11 11:34:21 +0100

    scss setup

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,8 +1,7 @@
 export default {
     template: `
         <header>
-            <span id="profile" class="fa-solid fa-circle-user fa-2x"></span>
-            <span id="setting" class="fa-solid fa-wrench fa-2x"></span>
+            
         </header>
     `
 }
