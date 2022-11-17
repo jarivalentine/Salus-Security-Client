e5569fb9b5be000e63a9d6725687385d6e7352f7
jari.valentine <jari.valentine@student.howest.be>
2022-11-17 11:40:16 +0100

    profile nav

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -2,14 +2,28 @@ export default {
     data() {
         return {
             firstname: 'Jarne Van Parys',
-            tag: 'Gardian Angel'
+            tag: 'Gardian Angel',
+            isActive: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.isActive = !this.isActive;
         }
     },
     template: `
         <header>
             <div>
-                <p>{{ firstname }}</p>
-                <p class="tag">{{ tag }}</p>
+                <div @click="toggleMenu">
+                    <p>{{ firstname }}</p>
+                    <p class="tag">{{ tag }}</p>
+                </div>
+                <ul :class="{ hidden: !isActive }">
+                    <li><a href="#">Profile</li>
+                    <li><a href="./history.html">History</a></li>
+                    <li><a href="#">Give feedback</li>
+                    <li><a href="#">Report a bug</li>
+                </ul>
             </div>
             <a href="./settings.html"></a>
         </header>
