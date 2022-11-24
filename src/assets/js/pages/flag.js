445af38014a68ed6269ea9f0c94212606a0a43a9
denis.topallaj <denis.topallaj13@gmail.com>
2022-11-24 19:17:23 +0100

    refactor date on ai page

--- src/assets/js/pages/flag.js
+++ src/assets/js/pages/flag.js
@@ -34,16 +34,7 @@ createApp({
             } else {
                 this.incidentValidated = "This incident is deemed invalid by the Salus Security AI";
             }
-            this.incidentDatetime = this.displayDate(this.incident["datetime"]);
-        },
-        displayDate(date){
-            const year = new Date(date).getFullYear();
-            const month = new Date(date).getMonth();
-            const day = new Date(date).getDay();
-            const hours = new Date(date).getHours();
-            const minutes = new Date(date).getMinutes();
-            const seconds = new Date(date).getSeconds();
-            return `${day}/${month}/${year} on ${hours}:${minutes}:${seconds}`;
+            this.incidentDatetime = `${new Date(this.incident["datetime"]).toLocaleDateString()} at ${new Date(this.incident["datetime"]).toLocaleTimeString()}`;
         },
         presentUsers(users){
             const listOfUsers = [];
