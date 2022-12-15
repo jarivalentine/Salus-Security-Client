eb895fed1b934f60b945faa501ff1bba97bf1f0b
denis.topallaj <denis.topallaj13@gmail.com>
2022-12-15 15:34:58 +0100

    radius on avatar, added border, css

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -3,21 +3,23 @@ export default {
         return {
             firstname: null,
             tag: 'Guardian Angel',
-            isActive: false
+            isActive: false,
         };
     },
     methods: {
-        toggleMenu() {
-            this.isActive = !this.isActive;
+        toggleMenu(e) {
+            if (e.target.closest("#menu") !== null || e.target.closest(".name-and-tag") !== null){
+                this.isActive = !this.isActive;
+            }
         },
         async changeName(){
             const user = await getOneUser(localStorage.getItem("userId"));
             this.firstname = `${user.firstname} ${user.lastname}`;
         },
         changePicture(){
-            const menu = document.querySelector("#menu");
             const userId = localStorage.getItem("userId");
-            menu.style.backgroundImage = `url("assets/img/avatars/${userId}.jpg")`;
+            const avatar = document.querySelector("#menu img");
+            avatar.src = `assets/img/avatars/${userId}.jpg`;
         }
     },
     async mounted() {
@@ -33,8 +35,11 @@ export default {
         <header>
             <div>
                 <div id="menu" @click="toggleMenu">
+                    <img src="assets/img/avatars/1989-01-28_AL.jpg" alt="avatar" title="avatar">
+                  <div class="name-and-tag">
                     <p>{{ firstname }}</p>
                     <p class="tag">{{ tag }}</p>
+                  </div>
                 </div>
                 <ul :class="{ hidden: !isActive }">
                     <li><a class="non-poc-menu" href="#">Profile</a></li>
