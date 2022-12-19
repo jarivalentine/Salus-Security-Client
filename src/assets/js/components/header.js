5265ba8e17d78bc9501ec1710da940a5707f2c37
brankodc <branko.de.caluwe@student.howest.be>
2022-12-19 12:12:43 +0100

    reactive TagNames

--- src/assets/js/components/header.js
+++ src/assets/js/components/header.js
@@ -1,8 +1,8 @@
 export default {
-    data() {
+    data() {   
         return {
             firstname: null,
-            tag: 'Guardian Angel',
+            tag: null,
             isActive: false,
         };
     },
@@ -15,11 +15,34 @@ export default {
         async changeName() {
             const user = await getOneUser(localStorage.getItem("userId"));
             this.firstname = `${user.firstname} ${user.lastname}`;
+
         },
         changePicture() {
             const userId = localStorage.getItem("userId");
             const avatar = document.querySelector("#menu");
             avatar.style.backgroundImage = `url('assets/img/avatars/${userId}.jpg')`;
+        },
+        async getTagName(){
+            const user = await getOneUser(localStorage.getItem("userId"));
+            const assists = await getAllHelpedIncidentsFromUser(user.id);
+            
+            if(assists.length<1){
+                this.tag = 'Bad Savior';
+            }
+            if(1<=assists.length&&assists.length<10){
+                this.tag = 'Noob Savior';
+            }
+            if(10<=assists.length&&assists.length<50){
+                this.tag = 'Great Savior';
+            }
+            if(50<=assists.length&&assists.length<100){
+                this.tag = 'Heroic Savior';
+            }
+            if(assists.length>=100){
+                this.tag = 'Legendary Savior';
+            }
+            
+            return this.tag;
         }
     },
     async mounted() {
@@ -29,7 +52,9 @@ export default {
             }
         });
         await this.changeName();
+        await this.getTagName();
         this.changePicture();
+        
     },
     template: `
       <header>
