a9040cf110528dee60eb453cb09f5fd30c29e2ae
jari.valentine <jari.valentine@student.howest.be>
2022-12-20 10:15:55 +0100

    changed main.js to module and moved global methods to api

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -134,3 +134,36 @@ function logError(error) {
 function call(request, successHandler, errorHandler) {
     fetch(request).then(successHandler).catch(errorHandler);
 }
+
+async function login(userId){ // console log function: change user
+    const invalid = "userId not recognized";
+    const valid = `logged is as user: ${userId}`;
+    if (!await validUserId(userId)){
+        console.log(invalid);
+        return;
+    }
+    localStorage.setItem("userId", userId);
+    console.log(valid);
+}
+
+async function validUserId(userId) {
+    const userIds = [];
+    const users = await getAllUsers();
+    users.map(index => userIds.push(index.id));
+    return userIds.includes(userId);
+}
+
+async function applyOrRemoveLockedMechanism(domElement){
+    const user = await getOneUser(localStorage.getItem("userId"));
+    const blurDomElement = document.querySelector(domElement);
+    if (blurDomElement === null){
+        return;
+    }
+    if (user.subscribed){
+        blurDomElement.classList.remove("locked");
+        return;
+    }
+
+    blurDomElement.classList.add("locked");
+}
+
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,5 @@
 import { subscribeToNotifications } from "./subscriptionHandler.js";
 
-let api;
-
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
@@ -18,36 +16,4 @@ function init() {
             }
         }
     });
-}
-
-async function login(userId){ // console log function: change user
-    const invalid = "userId not recognized";
-    const valid = `logged is as user: ${userId}`;
-    if (!await validUserId(userId)){
-        console.log(invalid);
-        return;
-    }
-    localStorage.setItem("userId", userId);
-    console.log(valid);
-}
-
-async function validUserId(userId) {
-    const userIds = [];
-    const users = await getAllUsers();
-    users.map(index => userIds.push(index.id));
-    return userIds.includes(userId);
-}
-
-async function applyOrRemoveLockedMechanism(domElement){
-    const user = await getOneUser(localStorage.getItem("userId"));
-    const blurDomElement = document.querySelector(domElement);
-    if (blurDomElement === null){
-        return;
-    }
-    if (user.subscribed){
-        blurDomElement.classList.remove("locked");
-        return;
-    }
-
-    blurDomElement.classList.add("locked");
 }
